2b3b421999d99cc917cd8fe531314d9a2218607d
TheGreatAxios <sawyer@dirtroad.dev>
2023-02-24 13:08:34 -0800

    Metaport V2 Upgrade

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,20 @@ function App() {
         openOnLoad: true, // Open Metaport on load (optional, default = false)
         openButton: false, // Show open/close action button (optional, default = true)
         autoLookup: false, // Automatic token lookup for M2S tokens (default = true)
-        skaleNetwork: 'staging', // SKALE network that will be used - mainnet or staging (optional, defualt = mainnet)  
+        skaleNetwork: 'staging3', // SKALE network that will be used - mainnet or staging (optional, defualt = mainnet)  
         chains: [
             'mainnet', // List of SKALE Chains that will be available in the Metaport UI (default = [])
-            'fancy-rasalhague',
-            'actual-secret-cebalrai'
+            'staging-legal-crazy-castor',
+            'staging-utter-unripe-menkar'
         ],
         chainsMetadata: { // Chain name aliases that will be displayed in the UI (optional, defualt = {})
             
-            'fancy-rasalhague': {
+            'staging-legal-crazy-castor': {
                 alias: 'Europa SKALE Chain', // optional
                 minSfuelWei: '27000000000000', // optional
                 faucetUrl: 'https://sfuel.dirtroad.dev', // optional
             },
-            'actual-secret-cebalrai': {
+            'staging-utter-unripe-menkar': {
                 alias: 'Calypso SKALE Chain',
                 minSfuelWei: '27000000000000', // optional
                 faucetUrl: 'https://sfuel.dirtroad.dev' // optional
@@ -31,21 +31,40 @@ function App() {
             'mainnet': {
                 'eth': {
                     'chains': [
-                        'fancy-rasalhague'
+                        'staging-legal-crazy-castor'
                     ]
                 }
             },
-            'fancy-rasalhague': {
+            'staging-legal-crazy-castor': {
                 'erc20': { 
-                    'ETHC': { // wrapper token
-                        'address': '0x201f24e897B0711dCBb21073a6A75F570a73fde4', // wrapper token address
-                        'name': 'ETHC', // wrapper token display name
-                        'symbol': 'ETHC',
+                    'wETH': { // wrapper token
+                        'address': '0xa270484784f043e159f74C03B691F80B6F6e3c24', // wrapper token address
+                        'name': 'ETH', // wrapper token display name
+                        'symbol': 'ETH',
                         'wraps': { // token that needs to be wrapped
                             'address': '0xD2Aaa00700000000000000000000000000000000', // unwrapped token address
-                            'symbol': 'wETHC', // unwrapped token symbol
-                            'iconUrl': '' // optional, icon URL for the origin token
+                            'symbol': 'ETH', // unwrapped token symbol
                         }
+                    },
+                    "wSKL": {
+                        "name": "SKL",
+                        "symbol": "SKL",
+                        "address": "0x6a679eF80aF3fE01A646F858Ca1e26D58b5430B6",
+                        "iconUrl": 'https://ruby.exchange/images/tokens/skl-square.jpg',
+                        'wraps': { // token that needs to be wrapped
+                            'address': '0xbA1E9BA7CDd4815Da6a51586bE56e8643d1bEAb6', // unwrapped token address
+                            'symbol': 'SKL' // unwrapped token symbol
+                          }
+                    },
+                    "wUSDC": {
+                        "name": "USDC",
+                        "symbol": "USDC",
+                        "address": "0x4f250cCE5b8B39caA96D1144b9A32E1c6a9f97b0",
+                        "iconUrl": 'https://ruby.exchange/images/tokens/usdc-square.jpg',
+                        'wraps': { // token that needs to be wrapped
+                            'address': '0x5d42495D417fcd9ECf42F3EA8a55FcEf44eD9B33', // unwrapped token address
+                            'symbol': 'USDC' // unwrapped token symbol
+                          }
                     }
                 }
             }
@@ -64,35 +83,49 @@ function App() {
             amount: "0.00005",
             chains: [
                 "mainnet", 
-                "fancy-rasalhague"
+                "staging-legal-crazy-castor"
             ],
             tokenType: dataclasses.TokenType.eth,
             lockValue: false,
             tokenKeyname: "eth"
         };
         
-        metaport.transfer(l1ToEuropaParams);
+        
+        metaport.transfer({
+            amount: "0.1",
+            chains: [
+                "mainnet", 
+                "staging-utter-unripe-menkar"
+            ],
+            tokenType: dataclasses.TokenType.eth,
+            tokenKeyname: "eth",
+            route: {
+                hub: "staging-legal-crazy-castor",
+                tokenKeyname:`_ETH_0xa270484784f043e159f74C03B691F80B6F6e3c24`,
+                tokenType: dataclasses.TokenType.erc20
+            }
+        })
         
         
         const l1ToEuropaResult = await handleEvent("transfer");
-        console.log("Result: ", l1ToEuropaResult);
+        // console.log("Result: ", l1ToEuropaResult);
         
         // Transfer from Europa -> Calypso
-        const wrapAndSendParams: interfaces.TransferParams = {
-            amount: "0.00005",
-            chains: [
-                "fancy-rasalhague",
-                "actual-secret-cebalrai"
-            ],
-            tokenType:  dataclasses.TokenType.erc20,
-            lockValue: true,
-            tokenKeyname: `_ETHC_0x201f24e897B0711dCBb21073a6A75F570a73fde4`
-        };
+        // const wrapAndSendParams: interfaces.TransferParams = {
+        //     amount: "0.00005",
+        //     chains: [
+        //         "fancy-rasalhague",
+        //         "actual-secret-cebalrai"
+        //     ],
+        //     tokenType:  dataclasses.TokenType.erc20,
+        //     lockValue: true,
+            // tokenKeyname: `_ETHC_0x201f24e897B0711dCBb21073a6A75F570a73fde4`
+        // };
         
-        metaport.transfer(wrapAndSendParams);
-        metaport.transfer(wrapAndSendParams);
-        const l2TransferResult = await handleEvent("transfer");
-        console.log("Result2: ", l2TransferResult);
+        // metaport.transfer(wrapAndSendParams);
+        // metaport.transfer(wrapAndSendParams);
+        // const l2TransferResult = await handleEvent("transfer");
+        // console.log("Result2: ", l2TransferResult);
         
         
     }
